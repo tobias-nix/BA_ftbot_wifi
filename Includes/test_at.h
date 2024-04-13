7f7e9dbed9930abb114202009d725655f35ca95e
Tobias Nix <tobias.nix@unibw.de>
2024-04-13 16:54:40 +0200

    Folder Change

--- Includes/test_at.h
+++
@@ -1,9 +0,0 @@
-#include "cmsis_os2.h"                  // CMSIS:RTOS2
-#include "main.h"
-#include "string.h"
-#include <stdbool.h>
-
-
-void setBlueLedIntervall(void);
-void sendAt(void);
-void setRedLed(bool);
--- Objects/init_send.d
+++
@@ -1,1 +0,0 @@
-./objects/init_send.o: init_send.c RTE\_Target_1\Pre_Include_Global.h
